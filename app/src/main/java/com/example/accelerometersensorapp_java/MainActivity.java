6d15d301cd590ac54706f0174b038be1bc0991cd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 11:41:50 +0530

    add a object of the class ImageView

--- app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
+++ app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.accelerometersensorapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
