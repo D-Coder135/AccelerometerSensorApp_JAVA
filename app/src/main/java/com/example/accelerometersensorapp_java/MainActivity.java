c7922e1b1dff3b5afa86ca33edeb68f6ad35e120
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 11:42:12 +0530

    add a object of the class MediaPlayer

--- app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
+++ app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.accelerometersensorapp_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
