d36c710625ae0f8ee5d3df0f9e8a3c9e09c70460
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 11:44:05 +0530

    initialize the object imageView

--- app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
+++ app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
@@ -20,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
     }
 }
