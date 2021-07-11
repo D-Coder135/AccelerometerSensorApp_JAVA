a9c1442995e7ea451bc6b3402ab7e0dd7e49b129
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 11:45:53 +0530

    initialize the object mediaPlayer with the method call of the method MediaPlayer.create()

--- app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
+++ app/src/main/java/com/example/accelerometersensorapp_java/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.accelerometersensorapp_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
@@ -24,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
         xaxis =  findViewById(R.id.textView);
         yaxis = findViewById(R.id.textView2);
         zaxis = findViewById(R.id.textView3);
+        mediaPlayer = MediaPlayer.create(this, R.raw.thunder);
     }
 }
